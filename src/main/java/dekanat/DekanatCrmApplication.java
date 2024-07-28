85cd3c6acda720d8748d893bd0d3a768843d48c4
DarkPoul <poulpotter1@gmail.com>
2024-07-28 09:14:06 +0300

    initial commit with dockerfile & docker-compose

---
+++ src/main/java/dekanat/DekanatCrmApplication.java
@@ -0,0 +1,13 @@
+package dekanat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DekanatCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DekanatCrmApplication.class, args);
+    }
+
+}
---
+++ src/main/java/dekanat/service/CustomUserDetailsService.java
@@ -0,0 +1,43 @@
+package dekanat.service;
+
+
+
+import dekanat.entity.UserEntity;
+import dekanat.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        assert userRepository != null;
+        Optional<UserEntity> userEntityOpt = userRepository.findByEmail(email);
+        if (userEntityOpt.isEmpty()) {
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+
+        UserEntity userEntity = userEntityOpt.get();
+        GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + userEntity.getRole());
+        return new User(userEntity.getEmail(), userEntity.getPassword(), Collections.singleton(authority));
+    }
+}
